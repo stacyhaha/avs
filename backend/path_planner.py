d97b221dd755387d05f8c03f06667e9ba9896ae7
zhyx <zhangyixiao2000@126.com>
2024-04-23 23:06:57 +0800

    add path_planner and some map data in a new new folder

--- backend/path_planner.py
+++ backend/path_planner.py
@@ -1,21 +1,141 @@
-from PIL import Image
+import numpy as np
+import heapq
+from functools import lru_cache
+from PIL import Image, ImageDraw
+from utils import *
 
-class PathPlanner:
+class Node:
+    def __init__(self, x, y, cost=0, parent=None):
+        self.x = x
+        self.y = y
+        self.cost = cost
+        self.parent = parent
+    
+    def __eq__(self, other):
+        # 比较节点是否相等，需要根据坐标来判断
+        return (self.x == other.x) and (self.y == other.y)
+    
+    def __hash__(self):
+        # 使得Node对象可哈希，以便在set中使用
+        return hash((self.x, self.y))
+    
+    def __lt__(self, other):
+        # 比较两个节点的成本，用于优先队列中的排序
+        return self.cost < other.cost
+    
+    def __add__(self, other):
+        # 实现节点的加法，便于计算移动后的新节点
+        return Node(self.x + other[0], self.y + other[1], self.cost, self)
+    
+    def __sub__(self, other):
+        # 用于计算两个节点之间的启发式距离
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
+class NodeQueue:
     def __init__(self):
-        # load map
-        return
+        self.elements = []
+    
+    def empty(self):
+        return len(self.elements) == 0
+    
+    def put(self, node):
+        heapq.heappush(self.elements, node)
+    
+    def get(self):
+        return heapq.heappop(self.elements)
 
-    def plan(self, current_loc, destination_loc):
-        """
-        current_loc: coordination in grid map 
-        destination_loc: coordination in grid map
-        for exmaple:
-        current_loc: (0, 2)
-        destination_loc: (2, 3)
 
-        output: the path list
-        for example: [(0, 2), (1, 2), (2, 2), (2, 3)]
-        """
+class AStar:
+    """A*算法"""
+    def __init__(self, start_pos, end_pos, map_array, move_step=3, move_direction=8):
+        self.map_array = map_array  # H * W
+        self.width = self.map_array.shape[1]
+        self.high = self.map_array.shape[0]
+        self.start = Node(*start_pos)  # 初始位置
+        self.end = Node(*end_pos)      # 结束位置
 
-        return [(0, 2), (1, 2), (2, 2), (2, 3)]
+        # Check if the start and end nodes are within the map boundaries
+        if not self._in_map(self.start) or not self._in_map(self.end):
+            raise ValueError(f"X coordinate range should be 0 to {self.width-1}, Y coordinate range should be 0 to {self.high-1}")
+        # Check if the start node is colliding with an obstacle
+        if self._is_collided(self.start):
+            raise ValueError("The starting node's x or y coordinate is on an obstacle")
+        # Check if the end node is colliding with an obstacle
+        if self._is_collided(self.end):
+            raise ValueError("The ending node's x or y coordinate is on an obstacle")
+
+        
+        self.reset(move_step, move_direction)
+
+    def reset(self, move_step=3, move_direction=8):
+        self.move_step = move_step
+        self.move_direction = move_direction
+        self.close_set = set()
+        self.open_queue = NodeQueue()
+        self.path_list = []
+
+    def _in_map(self, node):
+        return 0 <= node.x < self.width and 0 <= node.y < self.high
+    
+    def _is_collided(self, node):
+        return self.map_array[node.y, node.x] == 1  # 障碍物是1
+    
+    def _move(self):
+        @lru_cache(maxsize=3)
+        def _move_cached(move_step, move_direction):
+            moves = [
+                (0, move_step),        # 上
+                (0, -move_step),       # 下
+                (-move_step, 0),       # 左
+                (move_step, 0),        # 右
+                (move_step, move_step), # 右上
+                (move_step, -move_step), # 右下
+                (-move_step, move_step), # 左上
+                (-move_step, -move_step) # 左下
+            ]
+            return moves[:move_direction]
+        return _move_cached(self.move_step, self.move_direction)
+
+    def _update_open_list(self, curr):
+        for add in self._move():
+            next_ = curr + add
+            if not self._in_map(next_) or self._is_collided(next_) or next_ in self.close_set:
+                continue
+            H = next_ - self.end
+            next_.cost += H
+            self.open_queue.put(next_)
+            if H < 20:
+                self.move_step = 1
+
+    def __call__(self):
+        self.open_queue.put(self.start)
+        while not self.open_queue.empty():
+            curr = self.open_queue.get()
+            curr.cost -= (curr - self.end)
+            self._update_open_list(curr)
+            self.close_set.add(curr)
+            if curr == self.end:
+                break
+        while curr.parent is not None:
+            self.path_list.append(curr)
+            curr = curr.parent
+        self.path_list.reverse()
+        return self.path_list
+
+def main():
+    grid = np.loadtxt('../data/grid.txt', dtype=int)
+    planner = AStar(start_pos=(40, 40), end_pos=(270, 232), map_array=grid)
+    
+    path = planner()
+
+    image_path = '../data/Map.jpg'
+    output_path = '../data/Map_plan.jpg'
+    draw_path_on_image(image_path, path, output_path)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
     
--- backend/utils.py
+++ backend/utils.py
@@ -1,6 +1,7 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 from io import BytesIO
 import base64
+import numpy as np
 
 def transfer_loc_from_centi_to_grid_map(centi_loc):
     # return coordination in grid map
@@ -10,3 +11,49 @@ def decompress_base64_to_image(base64_data):
     image_data = base64.b64decode(base64_data)
     image = Image.open(BytesIO(image_data))
     return image
+
+def draw_path_on_image(image_path, path , output_path , line_color=(255, 0, 0), line_width=5):
+    image = Image.open(image_path)
+    
+    draw = ImageDraw.Draw(image)
+    #这里得按照cell的大小缩放一下
+    adjusted_path = [(node.x * 2, node.y * 2) for node in path]
+    draw.line(adjusted_path, fill=line_color, width=line_width)
+
+    image.save(output_path)
+    image.show()
+
+def identify_obstacles(image_path, grid_size):
+
+    #readme:
+    #image_path = 'Map_v1.jpg'
+    #grid_size = (255, 292)
+    #obstacle_grid = identify_obstacles(image_path, grid_size)
+
+    image = Image.open(image_path)
+    gray_image = image.convert('L')
+    gray_image.save('gray_image.jpg')  # 保存灰度图像
+
+    grid_height, grid_width = grid_size
+    print(grid_height, grid_width)
+    grid = np.zeros(grid_size, dtype=int)
+    
+    img_width, img_height = gray_image.size
+    cell_width = img_width // grid_width
+    cell_height = img_height // grid_height
+
+    for i in range(grid_height):
+        for j in range(grid_width):
+            left = j * cell_width
+            upper = i * cell_height
+            right = min((j + 1) * cell_width, img_width)
+            lower = min((i + 1) * cell_height, img_height)
+
+            cell = gray_image.crop((left, upper, right, lower))
+            non_road_pixels = sum(pixel < 200 for pixel in cell.getdata())
+
+            if non_road_pixels > 0.5 * (right-left) * (lower-upper):
+                grid[i, j] = 1
+
+    np.savetxt('grid.txt', grid, fmt="%d")  # 保存网格数据为文本文件
+    return grid
