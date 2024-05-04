15b8ef82aaa41d8eb6ed68f75a77244f899eb4db
Weizhi727 <130013595+Weizhi727@users.noreply.github.com>
2024-05-04 17:19:13 +0800

    Update predict.py

--- backend/object_detection/predict.py
+++ backend/object_detection/predict.py
@@ -1,195 +1,122 @@
-import torch
-import numpy as np
-import cv2
-from time import time
+import os
+
 from ultralytics import YOLO
 import matplotlib.pyplot as plt
-
+import cv2
 
 class ObjectDetection:
-
     def __init__(self):
-
-        # self.capture_index = capture_index
         self.device = 'mps'
-        self.model = self.load_model()
+        self.tf_checkpoint = '/Users/laiweizhi/PycharmProjects/5004Project/runs/train3/weights/best.pt'
+        self.ts_checkpoint = '/Users/laiweizhi/PycharmProjects/5004Project/ultralytics-main/runs/detect/train14/weights/best.pt'
+        # 導入traffic_light(tl) and traffic_sign(ts)的權重
+        self.model_tl = self.load_model(self.tf_checkpoint)
+        self.model_ts = self.load_model(self.ts_checkpoint)
 
-    def load_model(self):
-        model = YOLO('/Users/laiweizhi/PycharmProjects/5004Project/runs/train3/weights/best.pt')
+    def load_model(self, model_path):
+        model = YOLO(model_path)
         model.fuse()
         return model
 
-    def predict(self, frame):
-        results = self.model(frame)
+    def tl_predict(self, frame):
+        results = self.model_tl(frame)
         return results
 
-    def control_car(self, results):
-        # 邏輯大致可以像這樣
-        # 默認行為
-        action= '繼續行駛'
-        stop_threshold =200
-
-        for result in results:
-            boxes = result.boxes
-            name = result.names
-            class_name = name[boxes.cls.item()]
-            box = boxes.xyxy
-            box_size = (box[2] - box[0]) * (box[3] - box[1])
-
-            if class_name == 'red':  # 假设红灯的类别ID为2
-                action = "停车"
-            elif class_name == 'yellow':  # 假设黄灯的类别ID为1
-                if box_size > stop_threshold:
-                    action = "加速通过"
-                else:
-                    action = "减速并停车"
-            elif class_name == 'green':  # 假设绿灯的类别ID为3
-                action = "正常行驶"
-
-        print(f"执行动作: {action}")
-        return action
-
-    def plot_bboxes(self, results, frame):
-
-        def plot_bboxes(self, results, frame):
-            # 画出所有检测到的框
-            for result in results.xyxy[0]:
-                cv2.rectangle(frame, (int(result[0]), int(result[1])), (int(result[2]), int(result[3])), (0, 255, 0), 2)
-            cv2.imshow('frame', frame)
-        # xyxys= []
-        # confidences = []
-        # class_id = []
-        #
-        # for result in results:
-        #     boxes = result.boxes.cpu().numpy()
-        #
-        #     xyxys.append(result.xyxy)
-        #     confidences.append(result.conf)
-        #     class_id.append(result.cls)
-        #
-        #     # for xyxy in xyxys:
-        #         # confidences.append(xyxy[4])
-        #         # class_id.append(xyxy[5])
-        # # cv2.rectangle(frame, (int(xyxy[0]), int(xyxy[1])), (int(xyxy[2]), int(xyxy[3])), (0,255,0))
-        #
-        #
-        #
-        # return results[0].plot(), xyxys, confidences, class_id
+    def ts_predict(self, frame):
+        results = self.model_ts(frame)
+        return results
 
     def __call__(self, input_data):
-        results = self.predict(input_data)
-        class_lst = []
+        tl_results = self.tl_predict(input_data)
+        ts_results = self.ts_predict(input_data)
+        speed = 0
+        orientation = 0
+        bias = 0
 
-        for result in results:
+
+        detected_class = []
+
+        for result in tl_results:
             if len(result.boxes) == 0:
                 continue  # 如果没有检测到任何对象，跳过当前循环
 
             boxes = result.boxes
             names = result.names
 
-            # 处理多个检测结果
             for i in range(len(boxes)):
                 class_id = boxes.cls[i].item()  # 获取每个检测对象的类别ID
                 class_name = names[class_id]  # 从类别ID获取类别名称
-                class_lst.append(class_name)  # 将类别名称添加到列表
+                detected_class.append(class_name)  # 将类别名称添加到列表
 
-                print(f'detected sign is: {class_name}')
-                print(f'class type: {type(class_name)}')
+                print(f'tl detection: {detected_class}')
 
-            # 假设 result.plot() 为可视化检测结果的函数
-            processed_img = result.plot()
-            print(type(processed_img))
+                # print(f'detected class is: {class_name}')
+                # print(f'class type: {type(class_name)}')
 
-            if processed_img is not None:
-                processed_img = processed_img[:, :, [2, 1, 0]]  # 转换颜色通道顺序为RGB
-                plt.imshow(processed_img)
-                plt.axis('off')  # 关闭坐标轴
-                plt.show()
+            # processed_img = result.plot()
+            # print(type(processed_img))
+            #
+            # if processed_img is not None:
+            #     processed_img = processed_img[:, :, [2, 1, 0]]  # 转换颜色通道顺序为RGB
+            #     plt.imshow(processed_img)
+            #     plt.axis('off')  # 关闭坐标轴
+            #     plt.show()
 
-        return class_lst
-
-
-class Traffic_sign_detection:
-
-    def __init__(self):
-
-        # self.capture_index = capture_index
-        self.device = 'mps'
-        self.model = self.load_model()
-
-    def load_model(self):
-        model = YOLO('/Users/laiweizhi/PycharmProjects/5004Project/ultralytics-main/runs/detect/train14/weights/best.pt')
-        model.fuse()
-        return model
-
-    def predict(self, frame):
-        results = self.model(frame)
-        return results
-
-    def control_car(self, results):
-        # 邏輯大致可以像這樣
-        # 默認行為
-        action= '繼續行駛'
-        stop_threshold =200
-
-        for result in results:
-            if len(result) == 0:
-                continue
-            boxes = result.boxes
-            name = result.names
-            class_name = name[boxes.cls.item()]
-            box = boxes.xyxy
-            box_size = (box[2] - box[0]) * (box[3] - box[1])
-
-            if class_name == 'red':  # 假设红灯的类别ID为2
-                action = "停车"
-            elif class_name == 'yellow':  # 假设黄灯的类别ID为1
-                if box_size > stop_threshold:
-                    action = "加速通过"
-                else:
-                    action = "减速并停车"
-            elif class_name == 'green':  # 假设绿灯的类别ID为3
-                action = "正常行驶"
-
-        print(f"执行动作: {action}")
-        return action
-
-    def __call__(self, input_data):
-        results = self.predict(input_data)
-        class_lst = []
-
-        for result in results:
+        for result in ts_results:
             if len(result.boxes) == 0:
                 continue  # 如果没有检测到任何对象，跳过当前循环
 
             boxes = result.boxes
             names = result.names
 
-            # 处理多个检测结果
             for i in range(len(boxes)):
                 class_id = boxes.cls[i].item()  # 获取每个检测对象的类别ID
                 class_name = names[class_id]  # 从类别ID获取类别名称
-                class_lst.append(class_name)  # 将类别名称添加到列表
+                detected_class.append(class_name)  # 将类别名称添加到列表
 
-                print(f'detected sign is: {class_name}')
-                print(f'class type: {type(class_name)}')
+                print(f'ts detection: {detected_class}')
 
-            # 假设 result.plot() 为可视化检测结果的函数
-            processed_img = result.plot()
-            print(type(processed_img))
 
-            if processed_img is not None:
-                processed_img = processed_img[:, :, [2, 1, 0]]  # 转换颜色通道顺序为RGB
-                plt.imshow(processed_img)
-                plt.axis('off')  # 关闭坐标轴
-                plt.show()
+                # print(f'detected class is: {class_name}')
+                # print(f'class type: {type(class_name)}')
 
-        return class_lst
+            # processed_img = result.plot()
+            # # print(type(processed_img))
+            #
+            # if processed_img is not None:
+            #     processed_img = processed_img[:, :, [2, 1, 0]]  # 转换颜色通道顺序为RGB
+            #     plt.imshow(processed_img)
+            #     plt.axis('off')  # 关闭坐标轴
+            #     plt.show()
+
+        # print(f'class_lst is: {detected_class}, type is: {type(detected_class)}')
+        if 'red' in detected_class or 'stop' in detected_class:
+            speed = 0
+            orientation = 90
+            bias = 0
+        # else:
+        #     self.speed = 0
+        #     self.orientation = 0
+        #     self.bias = 0
+
+        return detected_class, (speed, orientation, bias)
+
+# red(減速再停車), yellow(減速), green(不進行動作)
+# go straight(不進行動作), limit 5 km(減速), parking place(執行停車), school area(減速並喇叭1秒), stop(停車), turn right(右轉), vehicle horn(喇叭1秒), zebra crossing(停車再行駛)
 
 if __name__ == '__main__':
-    # predict = ObjectDetection()
-    # output =predict('/Users/laiweizhi/Desktop/test_images')
-    # print(f'class_lst is: {output}, type is: {type(output)}')
-    predict = Traffic_sign_detection()
-    output = predict('/Users/laiweizhi/Desktop/test_images')
-    print(f'class_lst is: {output}, type is: {type(output)}')
+    model = ObjectDetection()
+    valid_extensions = {'jpeg', 'jpg', 'dng', 'bmp', 'mpo', 'webp', 'pfm', 'png', 'tiff', 'tif', 'mpg', 'avi', 'mp4',
+                        'wmv', 'mpeg', 'asf', 'webm', 'gif', 'ts', 'mkv', 'm4v', 'mov'}
+    directory = '/Users/laiweizhi/Desktop/object_detection_part/ts_test'
+    count = 1
+
+    for filename in os.listdir(directory):
+        ext = filename.split('.')[-1].lower()
+        if ext in valid_extensions:
+            file_path = os.path.join(directory, filename)
+            predict, action = model(file_path)
+            # print(predict)
+            print(action)
+            print(count)
+            # count+=1
